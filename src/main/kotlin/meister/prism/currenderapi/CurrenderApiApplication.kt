ba7032f048b0f1d71ba779517b8fc82b0932f223
sho0126hiro <sho0126hiro@gmail.com>
2019-11-10 00:39:24 +0900

    initial

---
+++ src/main/kotlin/meister/prism/currenderapi/CurrenderApiApplication.kt
@@ -0,0 +1,10 @@
+package meister.prism.currenderapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CurrenderApiApplication
+fun main(args: Array<String>) {
+	runApplication<CurrenderApiApplication>(*args)
+}
---
+++ src/test/kotlin/meister/prism/currenderapi/CurrenderApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package meister.prism.currenderapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CurrenderApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
